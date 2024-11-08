10f39fb6c9fcc9228d76a42a459c62b9b44745d0
Ederson Abreu <edersonabreu@icloud.com>
2024-11-07 23:18:47 -0300

    graph page

--- src/app/module/aplication/monthly-contribution/monthly-contribution.component.ts
+++ src/app/module/aplication/monthly-contribution/monthly-contribution.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as echarts from 'echarts';
 
 @Component({
   selector: 'app-monthly-contribution',
@@ -27,6 +28,61 @@ export class MonthlyContributionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.initChart();
+  }
+
+  initChart(): void {
+    const chartDom = document.getElementById('graph');
+    const myChart = echarts.init(chartDom!);
+
+    const option = {
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b}: R$ {c} ({d}%)'
+      },
+      legend: {
+        show: false
+      },
+      series: [
+        {
+          name: 'Contribuições',
+          type: 'pie',
+          radius: ['50%', '70%'],
+          avoidLabelOverlap: false,
+          label: {
+            show: false,
+            position: 'outside',
+            formatter: '{b}: R$ {c}'
+          },
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          },
+          data: [
+            {
+              value: 499999.99,
+              name: 'Contribuição Mensal',
+              itemStyle: {
+                color: '#594CBE'  
+              }
+            },
+            {
+              value: 499999.99,
+              name: 'Contribuição Voluntária',
+              itemStyle: {
+                color: '#E22E6F'
+              }
+            }
+          ]
+        }
+      ]
+    };
+
+    myChart.setOption(option);
+    myChart.resize();
   }
 
 }
